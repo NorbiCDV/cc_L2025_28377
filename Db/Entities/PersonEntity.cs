1d374e412b9bce314f52ac982d7131d9c4d8f17b
NorbiCDV <166927698+NorbiCDV@users.noreply.github.com>
2025-03-29 14:30:50 +0100

    Add files via upload

---
+++ Db/Entities/PersonEntity.cs
@@ -0,0 +1,13 @@
+namespace CDV.PeopleApi
+{
+    public class PersonEntity
+    {
+
+        public int Id {get; set; }
+
+        public string FirstName {get; set; }
+        public string LastNameName {get; set; }
+
+        public string? PhoneNumber {get; set;} 
+    }
+}
---
+++ Db/PeopleDb.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace CDV.PeopleApi
+{
+    public class PeopleDb : DbContext
+    {
+        public PeopleDb(DbContextOptions<PeopleDb> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var PersonEntity = modelBuilder.Entity<PersonEntity>();
+
+            PersonEntity.ToTable("Person");
+            PersonEntity.HasKey(i=>i.Id);
+
+            PersonEntity.Property(p=>p.FirstName).HasMaxLength(250);
+            PersonEntity.Property(p=>p.LastNameName).HasMaxLength(250);
+            PersonEntity.Property(p=>p.PhoneNumber).HasMaxLength(12);
+        }
+
+        public DbSet<PersonEntity> People {get; set;}
+    }
+}
